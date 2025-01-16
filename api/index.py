3b4b6e4695c5ae695d6e98d8d233567103a13cfe
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2025-01-17 06:02:57 +0700

    Fix vercel app entry point

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-from django.core.wsgi import get_wsgi_application
+from ..DocHunter.wsgi import application
 
-# This is the WSGI application used by Django's development server and any production WSGI deployments. It should not be used with any ASGI deployments.
-app = get_wsgi_application()
+app = application

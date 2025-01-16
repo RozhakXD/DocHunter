36d65502dcc94926a1e81752c1214d0f3d775338
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2025-01-17 05:55:43 +0700

    Fix Vercel deployment with "api/index.py"

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from django.core.wsgi import get_wsgi_application
+
+# This is the WSGI application used by Django's development server and any production WSGI deployments. It should not be used with any ASGI deployments.
+app = get_wsgi_application()
